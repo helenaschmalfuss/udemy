47c4d0420b0994bfff4809aa49e36414644128ce
helenaschmalfuss <helenaschmalf@gmail.com>
2025-04-18 17:11:14 -0300

    Add files via upload

--- task.py
+++ task.py
@@ -1,43 +1,8 @@
-print(r'''
-*******************************************************************************
-          |                   |                  |                     |
- _________|________________.=""_;=.______________|_____________________|_______
-|                   |  ,-"_,=""     `"=.|                  |
-|___________________|__"=._o`"-._        `"=.______________|___________________
-          |                `"=._o`"=._      _`"=._                     |
- _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
-|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
-|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
-          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
- _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
-|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
-|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
-____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
-/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
-____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
-/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
-____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
-/______/______/______/______/______/______/______/______/______/______/_____ /
-*******************************************************************************
-''')
-print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.")
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
 
-direction = input('You are at a cross road. Where do you want to go? \nType "left" or "right". ')
+total_bill = ((bill) + (bill*tip/100)) / (people)
 
-if direction.lower() == "left":
-    print('You have come to a lake. There is an island in the middle of the lake.')
-    boat = input('Type "wait" to wait for a boat. Type "swim" to swim across.')
-    if boat.lower() == "wait":
-        print('You arrive at the island unharmed. There is a house with 3 doors. One red, onde yellow and one blue.')
-        door = input('Which colour do you choose? ')
-        if door.lower() == "red":
-            print('It is a room full of fire. Game Over.')
-        elif door.lower() == "blue":
-            print('You enter a room of beasts. Game Over.')
-        else:
-            print('You found the treasure. You win!')
-    else:
-        print('You get attacked by an angry trout. Game Over.')
-else:
-    print('You fell into a hole. Game Over.')
+print(f'Each person should pay: ${total_bill}' )
